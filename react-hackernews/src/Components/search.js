a822aa432ecead17bd55120d0676bcc802d0a35c
Wisdom Liu <wisdom.code161@gmail.com>
2019-06-23 19:15:26 -0700

    applied focus for search input

--- react-hackernews/src/Components/search.js
+++ react-hackernews/src/Components/search.js
@@ -1,21 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Search = ({ 
-  value, 
-  onChange,
-  onSearchSubmit,
-  children  
-}) =>
-  <form onSubmit={onSearchSubmit}>
-    <input
-      type="text"
-      value={value}
-      onChange={onChange}
-    />
-    <button 
-      type="submit">
-      {children}
-    </button>
-  </form>
+// const Search = ({ 
+//   value, 
+//   onChange,
+//   onSearchSubmit,
+//   children  
+// }) => {
+//   let input;
+//   return (
+//   <form onSubmit={onSearchSubmit}>
+//     <input
+//       type="text"
+//       value={value}
+//       onChange={onChange}
+//       ref={el => this.input = el}
+//     />
+//     <button 
+//       type="submit">
+//       {children}
+//     </button>
+//   </form>
+//   );
+// }
 
+class Search extends Component {
+  componentDidMount() {
+    if(this.input) {
+      this.input.focus();
+    }
+  }
+
+  render() {
+    const {
+      value,
+      onChange,
+      onSearchSubmit,
+      children
+    } = this.props;
+
+    return(
+     <form onSubmit={onSearchSubmit}>
+      <input
+        type="text"
+        value={value}
+        onChange={onChange}
+        ref={el => this.input = el}
+      />
+      <button
+        type="submit">
+        {children}
+      </button>
+     </form>
+    )
+  }
+}
 export default Search;
