8adb60fb3525dd3873faee4647c86a8c7e5025b6
Wisdom Liu <wisdom.code161@gmail.com>
2019-06-23 18:45:59 -0700

    replaced fetch with axios

--- react-hackernews/src/App.js
+++ react-hackernews/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './styles/App.css';
 import Search from './Components/search';
 import Table from './Components/table';
@@ -56,9 +57,8 @@ class App extends Component {
   }
   
   fetchSearchTopStories(searchTerm, page = 0) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
-      .then(response => response.json())
-      .then(result => this.setSearchTopStories(result))
+    axios(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+      .then(result => this.setSearchTopStories(result.data))
       .catch(error => this.setState({ error }) )
   }
 
